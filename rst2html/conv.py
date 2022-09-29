eac2c6f430d04e27d61ea85a9069ea6001b0f02e
Zhu Qi <zhuqi@realtek-sg.com>
2022-09-29 10:10:40 +0800

    [Update] Script minor bug fix - fixed appendSplit2Template() running twice issue
    Verification:
    Current script is working on Windows VSCode Python environment

--- rst2html/conv.py
+++ rst2html/conv.py
@@ -174,61 +174,50 @@ def splitHTMLContent():
 
 # filter content-only in introduction & procedure section
 def processSplitHTML():
-    for file in os.listdir(root_path):
-        if file.endswith("intro.html"):
-            # print(file)
-            soup = BeautifulSoup(
-                open((os.path.join(root_path, file)), encoding="utf-8"), "html.parser")
+    for filename in os.listdir(root_path):
+        if filename.endswith("intro.html"):
+            print(filename)
+            soup = BeautifulSoup(open((os.path.join(root_path, filename)), encoding="utf-8"), "html.parser")
             introduction_content = soup.find_all('p')  # filter content area
-            introduction_content = BeautifulSoup(
-                str(introduction_content), 'html.parser').prettify()
-            with open("intro.txt", "w+", encoding="utf-8") as file:
+            introduction_content = BeautifulSoup(str(introduction_content), 'html.parser').prettify()
+            with open((os.path.join(root_path, filename)), "w+", encoding="utf-8") as file:
                 # remove start and end unnecessary part
                 write_content = ''.join(str(introduction_content)[1:-2])
-                write_content = write_content.replace(
-                    "<p>", "  ")  # remove <p>
-                write_content = write_content.replace(
-                    "</p>", "  ")  # remove </p>
+                write_content = write_content.replace("<p>", "  ")  # remove <p>
+                write_content = write_content.replace("</p>", "  ")  # remove </p>
                 file.write(write_content)
             file.close()
-    with open("intro.txt", "r", encoding="utf-8") as input, open('intro.txt', 'r+') as output:
-        for line in input:
-            if line.startswith(",") != 0:
-                line = ''
-                continue
-            if line.strip():
-                output.write(line)
-        output.truncate()
-
-    for file in os.listdir(root_path):
-        if file.endswith("procedure.html"):
-            soup = BeautifulSoup(open((os.path.join(root_path, file)), encoding="utf-8"), "html.parser").find_all(['p', 'blockquote'])
-            # soup = soup + BeautifulSoup(open((os.path.join(root_path, file)), encoding="utf-8"), "html.parser").find_all('blockquote')
-            procedure_content = BeautifulSoup(
-                str(soup), 'html.parser').prettify()
-            # print(procedure_content)
-            with open("proce.html", "w+", encoding="utf-8") as file:
+            with open((os.path.join(root_path, filename)), "r", encoding="utf-8") as input, open((os.path.join(root_path, filename)), 'r+') as output:
+                for line in input:
+                    if line.startswith(",") != 0:
+                        line = ''
+                        continue
+                    if line.strip():
+                        output.write(line)
+                output.truncate()
+            
+        if filename.endswith("proce.html"):
+            print(filename)
+            soup = BeautifulSoup(open((os.path.join(root_path, filename)), encoding="utf-8"), "html.parser").find_all(['p', 'blockquote'])
+            procedure_content = BeautifulSoup(str(soup), 'html.parser').prettify()
+            with open((os.path.join(root_path, filename)), "w+", encoding="utf-8") as file:
                 write_content = ''.join(str(procedure_content)[1:-2])
                 write_content = write_content.replace("<p>", "  ")   # remove <p>
                 write_content = write_content.replace("</p>", "  ")  # remove </p>
                 write_content = write_content.replace("<tt class=\"docutils literal\">", "  ") 
-                write_content = write_content.replace(" <tt class=\"docutils\">", "  ") 
-                write_content = write_content.replace("   <tt class=\"docutils\">", "  ")
-                write_content = write_content.replace("     <tt class=\"docutils\">", "  ")
                 write_content = write_content.replace("</tt>", "  ") 
                 file.write(write_content)
             file.close()
-    
-    
-    for file in os.listdir(root_path):
-        if file.endswith("proce.html"):
-            soup = BeautifulSoup(open((os.path.join(root_path, file)), encoding="utf-8"), "html.parser")
+            
+        if filename.endswith("proce.html"):
+            print(filename)
+            soup = BeautifulSoup(open((os.path.join(root_path, filename)), encoding="utf-8"), "html.parser")
             img_hearders = soup.find_all("img")
             link_headers = soup.find_all("a")
             span_headers = soup.select("span")
             strong_headers = soup.find_all("strong")
             count = 0
-
+            
             for img_header in img_hearders:
                 count = count + 1
                 img_header['class'] = "size-full wp-image-3851 aligncenter"
@@ -237,48 +226,53 @@ def processSplitHTML():
                 del img_header['style']
                 img_header['width'] = "800"
                 img_header['height'] = "500"
-
             for link_header in link_headers:
                 del link_header ["class"]
-
             for span_header in span_headers:
                 span_header.extract()
             last_header = "Code Reference"
-
             for last_header in strong_headers:pass
             if last_header:
-                print(last_header)
                 last_header["style"] = "color: #e67e22; background-color: white; font-size: 28px"
                 last_header.name = "span"
             else:
                 break
             soup = BeautifulSoup(str(soup), 'html.parser').prettify()
-            with open("proce.html", "w+", encoding="utf-8") as file:
+            with open((os.path.join(root_path, filename)), "w+", encoding="utf-8") as file:
                 write_content = ''.join(str(soup))
                 file.write(write_content)
             file.close()
-    
-    with open("proce.html", "r", encoding="utf-8") as input, open('proce.html', 'r+', encoding="utf-8") as output:
-        for line in input:
-            if line.startswith(",") != 0 or line.startswith(" ,") != 0 or line.startswith("<p style=\"color:#E67E22; font-size:24px\">") != 0 or line.startswith("</p>") != 0:
-                line = ''
-                continue
-            # if line.startswith("<p style=\"color:#E67E22; font-size:24px\">") != 0:
-            #     print(line)
-            if line.strip():
-                output.write(line)
-        output.truncate()
+            
+            with open((os.path.join(root_path, filename)), "r", encoding="utf-8") as input, open((os.path.join(root_path, filename)), 'r+', encoding="utf-8") as output:
+                for line in input:
+                    if line.startswith(",") != 0 or line.startswith(" ,") != 0 or line.startswith("<p style=\"color:#E67E22; font-size:24px\">") != 0 or line.startswith("</p>") != 0 or line.startswith("<blockquote>")!=0 or line.startswith("</blockquote>")!=0:
+                        line = ''
+                        continue
+                    if line.strip():
+                        output.write(line)
+                output.truncate()
     
 # append to template
 def appendSplit2Template():
-    temp = root_path + "\\template\\mindy.html"
+    temp = root_path + "\\template_self\\mindy.html"
     output_dir = root_path + "\\output\\"
+    
+    #check if output folder exist
+    output_folder = os.path.exists(output_dir)
+    if output_folder == True:
+        print(output_folder)
+        print ("%s has already been created" % output_dir)
+    else:
+        os.mkdir(output_dir)
+        print ("%s not existed, created a new one" % output_dir)
+    
     
     for file in os.listdir(root_path):
         if file.endswith("intro.html"):
+            print("11111111111 %s", file)
             if os.path.isfile(file):
                 filename = file.split('_')[0]
-                print("[INFO] Currently processing '%s_intro.html'..." % filename)
+                print("[%s][INFO] Currently processing '%s_intro.html'..." % ("appendSplit2Template", filename))
                 # create a new folder to store output files
                 if os.path.isdir(output_dir):
                     shutil.copy(temp , output_dir + filename + ".html")
@@ -293,6 +287,7 @@ def appendSplit2Template():
                             file_output.seek(pos)
 
                             with open(file, "r", encoding="utf-8") as f1:
+                                print(file)
                                 for line in f1:
                                     print(line)
                                     file_output.write(line)
@@ -301,10 +296,8 @@ def appendSplit2Template():
                             print(pos)
                         f1.close()
                         file_output.close()
-                else:
-                    os.mkdir(output_dir)
-            print("===========================================")
-
+    print("===========================================")
+    print("[%s][INFO] added intro.html to the template" % ("appendSplit2Template"))
             
     for file in os.listdir(root_path):
         if file.endswith("proce.html"):
@@ -323,6 +316,7 @@ def appendSplit2Template():
                         file_output.seek(pos)
 
                         with open(file, "r", encoding="utf-8") as f1:
+                            print(file)
                             for line in f1:
                                 print(line)
                                 file_output.write(line)
@@ -333,8 +327,9 @@ def appendSplit2Template():
                     file_output.close()
                 # else:
                     # os.mkdir(output_dir)
-            print("===========================================")
-
+    print("===========================================")
+    print("[%s][INFO] added proce.html to the template" % ("appendSplit2Template"))
+    
 # remove processing middle files
 def removeFiles():
     output_dir = root_path + "\\output\\"
@@ -357,9 +352,9 @@ def removeFiles():
 if __name__ == '__main__':
     print("===== [MAIN Function] =====")
 
-    formatRST()
+    # formatRST()
     convRST2HTML()
-    splitHTMLContent()
-    processSplitHTML()
-    appendSplit2Template() #pending to debug 
-    removeFiles() #pending to debug 
+    # splitHTMLContent()
+    # processSplitHTML()
+    # appendSplit2Template()
+    # removeFiles() #pending to debug 
